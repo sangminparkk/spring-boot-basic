6120583d6b4e6757bea84664059772788980c99c
chandler <parksm0529@gmail.com>
2024-08-05 23:00:33 +0900

    feature : controller 기본맵핑 검증 - 적용 : MockMvc

---
+++ src/main/java/me/chandler/springbootbasic/controller/AccountController.java
@@ -0,0 +1,13 @@
+package me.chandler.springbootbasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/")
+    public String greeting() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/me/chandler/springbootbasic/controller/AccountControllerTest.java
@@ -0,0 +1,33 @@
+package me.chandler.springbootbasic.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/요청시 Hello World 출력")
+    public void print_Hello_World() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andDo(print());
+    }
+
+}
